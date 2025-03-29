31cedfeea03c151941c795993168da2952a27859
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2025-03-29 10:22:01 +0530

    added csrf

--- django_postgres_project/settings.py
+++ django_postgres_project/settings.py
@@ -124,3 +124,9 @@ STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://djangopostgres-production.up.railway.app/"
+]
