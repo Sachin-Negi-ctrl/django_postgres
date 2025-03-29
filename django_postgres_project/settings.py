d17abb1c57ff80ad3a218158856bf9ea930f638b
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2025-03-29 10:05:01 +0530

    Second commit

--- django_postgres_project/settings.py
+++ django_postgres_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b_t^gac7vz+lta*1_v&v4$71+z*)%1e_6agtaj8mzm=4f91fw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
